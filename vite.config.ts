a3526c3f14afbde228ee8fc8f08d8168c0cb0914
leo <liubo@cjitech.com>
2025-12-26 17:44:12 +0800

    禁用编辑

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,20 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) return "vendor";
+        },
+      },
+    },
+  },
 });
